2dacbf110a119d38fb79d0942a005921cd83d90d
Sambhav Jain <136801346+DarkLord017@users.noreply.github.com>
2026-03-07 19:35:35 +0530

    api and server setup

--- data-explorer/integration/contracts/setup.sh
+++ data-explorer/integration/contracts/setup.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Base dir: $SCRIPT_DIR"
 
 solc --optimize --optimize-runs 200 \
---abi --bin "$SCRIPT_DIR/Storage.sol" -o "$SCRIPT_DIR/build"
+--abi --bin --overwrite "$SCRIPT_DIR/Storage.sol" -o "$SCRIPT_DIR/build"
 
 abigen \
 --abi "$SCRIPT_DIR/build/Storage.abi" \
